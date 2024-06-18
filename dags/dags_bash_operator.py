d08ab54877aa92d25a23cadddd5abae99220b694
HyunBeomJoo <apqpqi2163@gmail.com>
2024-06-18 20:50:19 +0900

    change datetime in dags_bash_operator.py

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -6,13 +6,13 @@ from airflow.operators.bash import BashOperator
 with DAG(
     dag_id="dags_bash_operator",
     schedule="0 0 * * *",
-    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2024, 6, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
     bash_t1 = BashOperator(
         task_id="bash_t1",
         bash_command="echo whoami",
-    )
+    ) 
 
     bash_t2 = BashOperator(
         task_id="bash_t2",
