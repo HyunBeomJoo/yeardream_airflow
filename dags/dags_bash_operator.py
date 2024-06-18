340e57e9adcc23892ee5288fb20691d286434481
HyunBeomJoo <apqpqi2163@gmail.com>
2024-06-18 20:51:47 +0900

    fix

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -7,7 +7,7 @@ with DAG(
     dag_id="dags_bash_operator",
     schedule="0 0 * * *",
     start_date=pendulum.datetime(2024, 6, 1, tz="Asia/Seoul"),
-    catchup=False
+    catchup=True
 ) as dag:
     bash_t1 = BashOperator(
         task_id="bash_t1",
