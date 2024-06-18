69d0c4af5520a0021b25249d133a81fa49a366f7
HyunBeomJoo <apqpqi2163@gmail.com>
2024-06-18 11:16:01 +0900

    fix bikelist.py

--- dags/dags_seoul_bikelist.py
+++ dags/dags_seoul_bikelist.py
@@ -1,59 +1,17 @@
-from airflow.models.baseoperator import BaseOperator
-from airflow.hooks.base import BaseHook
-import pandas as pd 
+from operators.seoul_api_to_csv_operator import SeoulApiToCsvOperator
+from airflow import DAG
+import pendulum
 
-class SeoulApiToCsvOperator(BaseOperator):
-    template_fields = ('endpoint', 'path','file_name','base_dt')
-
-    def __init__(self, dataset_nm, path, file_name, base_dt=None, **kwargs):
-        super().__init__(**kwargs)
-        self.http_conn_id = 'openapi.seoul.go.kr'
-        self.path = path
-        self.file_name = file_name
-        self.endpoint = '{{var.value.apikey_openapi_seoul_go_kr}}/json/' + dataset_nm
-        self.base_dt = base_dt
-
-    def execute(self, context):
-        import os
-        
-        connection = BaseHook.get_connection(self.http_conn_id)
-        self.base_url = f'http://{connection.host}:{connection.port}/{self.endpoint}'
-
-        total_row_df = pd.DataFrame()
-        start_row = 1
-        end_row = 1000
-        while True:
-            self.log.info(f'시작:{start_row}')
-            self.log.info(f'끝:{end_row}')
-            row_df = self._call_api(self.base_url, start_row, end_row)
-            total_row_df = pd.concat([total_row_df, row_df])
-            if len(row_df) < 1000:
-                break
-            else:
-                start_row = end_row + 1
-                end_row += 1000
-
-        if not os.path.exists(self.path):
-            os.system(f'mkdir -p {self.path}')
-        total_row_df.to_csv(self.path + '/' + self.file_name, encoding='utf-8', index=False)
-
-    def _call_api(self, base_url, start_row, end_row):
-        import requests
-        import json 
-
-        headers = {'Content-Type': 'application/json',
-                   'charset': 'utf-8',
-                   'Accept': '*/*'
-                   }
-
-        request_url = f'{base_url}/{start_row}/{end_row}/'
-        if self.base_dt is not None:
-            request_url = f'{base_url}/{start_row}/{end_row}/{self.base_dt}'
-        response = requests.get(request_url, headers)
-        contents = json.loads(response.text)
-
-        key_nm = list(contents.keys())[0]
-        row_data = contents.get(key_nm).get('row')
-        row_df = pd.DataFrame(row_data)
-
-        return row_df
+with DAG(
+    dag_id='dags_seoul_bikelist',
+    schedule='0 7 * * *',
+    start_date=pendulum.datetime(2024,6,16, tz='Asia/Seoul'),
+    catchup=False
+) as dag:
+    '''서울시 공공자전거 실시간 대여 현황'''
+    seoul_api2csv_bike_list = SeoulApiToCsvOperator(
+        task_id='seoul_api2csv_bike_list',
+        dataset_nm='bikeList',
+        path='/opt/airflow/ingest/bikeList/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
+        file_name='bikeList.csv'
+    )
