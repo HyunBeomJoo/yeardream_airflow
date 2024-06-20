8208b20f6e9d673799587200888b0275f0413825
HyunBeomJoo <apqpqi2163@gmail.com>
2024-06-20 20:51:23 +0900

    s

--- dags/dags_python_decorator_with_param.py
+++ dags/dags_python_decorator_with_param.py
@@ -21,4 +21,9 @@ with DAG(
         from pprint import pprint
         pprint(kwargs)
 
-    python_task_1 = regist3('hjkim', 'man', 'seoul', email='hjkim_sun@naver.com', phone='010')
+        data_interval_start = kwargs.get('data_interval_start')
+        data_interval_end = kwargs.get('data_interval_end')
+        print(data_interval_start)
+        print(data_interval_end)
+
+python_task_1 = regist3('hbjoo', 'man', 'suwon', email='apqpqi2163@gmail.com', phone='010')
